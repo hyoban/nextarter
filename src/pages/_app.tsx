a6ab0df9ab4f069da4702018b2f751aa65f3adac
hyoban <hi@hyoban.cc>
2023-02-20 20:05:03 +0800

    chore: use next-theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import { api } from "@/lib/utils";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "next-themes";
 import { type AppType } from "next/app";
 import Head from "next/head";
 
@@ -11,14 +12,16 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Head>
-        <title>Create T3 App</title>
-      </Head>
-      <main className="h-full w-full">
-        <Component {...pageProps} />
-      </main>
-    </SessionProvider>
+    <ThemeProvider>
+      <SessionProvider session={session}>
+        <Head>
+          <title>Create T3 App</title>
+        </Head>
+        <main className="h-full w-full">
+          <Component {...pageProps} />
+        </main>
+      </SessionProvider>
+    </ThemeProvider>
   );
 };
 
